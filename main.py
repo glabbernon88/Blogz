9dc59f1f22628d7d68de5fa56ce7d16591f2f394
Diane Lee <dplee86@gmail.com>
2017-10-17 19:21:55 -0500

    Final copy of build a blog

--- main.py
+++ main.py
@@ -23,18 +23,11 @@ class Blogpost(db.Model):
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blogpost():
-    #displays all the blog posts
-    if request.method == 'POST':    #this does not do anything because this route is always GET
-        title = request.form['title'] 
-        body = request.form['body']
-        blog_post = Blogpost(title, body)
-    
-     
     try:                            #try the following argument and will execute if it works, if not, it will go to except stmt
         id = request.args['id']         #sets the variable id = requests an item "id" from the dictionary args
         posts = Blogpost.query.filter_by(id=id).first() #set the variable posts to return the id - first one
-        posts = [posts]                                 #turns variable posts into a list (for the templates page)
-        return render_template('blogmain.html', title="Build a Blog",posts = posts) #returns the template blogmain.html with title Build a Blog, and the posts variable value
+        posts = [posts]                         #turns variable posts into a list (for the blogmain template page)         
+        return render_template('blogview.html', title="Build a Blog" ,posts = posts) #returns the template blogmain.html with blog title and the posts variable value
     except:                                                 #if the try statement does not work, this will execute
         posts = Blogpost.query.all()                        # returns a list of all the blogs
         return render_template('blogmain.html',title="Build a Blog", posts = posts)  #returns template blogmain.html with a list of all blogs
@@ -46,25 +39,28 @@ def index():
     if request.method == 'POST':                #if request method is POST, the following statements are executed
         blog_title = request.form['title']      #sets the variable blog_title from the template
         blogpost = request.form['body']         #sets the variable blogpost from the template
-        # Verify title and body are not empty
-        #if either is empty return an error message
-        #redirect ? to post to new post page        
+        if blog_title == "" and blogpost == "":
+            flash("Please fill in the title and body", 'error')
+            return render_template('newpost.html')
         if blog_title == "":                    #if no title is entered, error message is returned
-           session['blog_title'] = blog_title
-           flash("Please fill in the title",'error')
+            flash("Please fill in the title",'error')
+            return render_template('newpost.html', body = blogpost)
         if blogpost == "":                      # if no body is entered, error message is returned
-            session['blogpost']= blogpost       
+            # session['blogpost']= blogpost       
             flash("Please fill in the body",'error')
-            return redirect('/newpost')        #redirects to /newpost page
-        else:     
+            return render_template('newpost.html', title = blog_title)
+        else:
             new_post = Blogpost(blog_title, blogpost)   #if no errors, the following is executed
             db.session.add(new_post)                    #creates a "staging" of the blogpost entry
             db.session.commit()                         #commits the entry to the database
-            
-    return render_template('newpost.html')          #returns the newpost template
+            id = new_post.id                            #creates variable id from the new_post to use in the redirect stmt
+            posts = [new_post]                          #turns the variable posts in into a list, which is iterable
+            return redirect('/blog?id='+str(id))        #to redirect to the main blog page and display the individual blog
+            return render_template('blogview.html', id=id, title = "Build a Blog", posts = posts)     #returns to main blog page -want to display the individual blog page
 
-
-
+    return render_template('newpost.html')          #returns the newpost templ.ate
+    
+    
     
 if __name__ == '__main__':
     app.run()
